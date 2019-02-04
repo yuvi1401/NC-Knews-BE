6dec09434c0a2f639e058befb5547aa1e0d0d257
yuvaraj kadam <yuvarajkadam@macusers-iMac-2.local>
2019-02-04 16:11:22 +0000

    index file update

--- error/index.js
+++ error/index.js
@@ -1,21 +1,19 @@
 exports.handle400 = (err, req, res, next) => {
-  // console.log(err);
   const { code } = err;
-  // console.log(code);
+
   const codes = {
     42703: 'incorrect input post request cannot be processed',
     23503: 'key is not present in the source table',
     23505: 'key value already exist',
     '22P02': 'Invalid input syntax',
   };
-  // console.log(codes[code]);
+
   if (codes[code] || err.status === 400) {
     res.status(400).send({ message: codes[code] });
   } else next(err);
 };
 
 exports.handle404 = (err, req, res, next) => {
-  // console.log(err);
   if (err.status === 404) res.status(404).send({ message: 'Not found' });
   else next(err);
 };
